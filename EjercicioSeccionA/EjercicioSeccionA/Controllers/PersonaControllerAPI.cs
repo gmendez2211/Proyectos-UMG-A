1c231de9041aa16844862a090fa95b60ccc00156
Gustavo Mendez <gmendez.2211@gmail.com>
2021-10-10 10:39:08 -0600

    Se agrega PersonaControllerAPI
     PersonaControllerAPI  contiene método para listar personas y se muestran a través de un json

---
+++ EjercicioSeccionA/EjercicioSeccionA/Controllers/PersonaControllerAPI.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using EjercicioSeccionA.Repository.iRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjercicioSeccionA.Controllers
+{
+    [Route("api/Persona")]
+    [ApiController]
+    public class PersonaControllerAPI : Controller
+    {
+        private readonly iPersonaRepository _ctopersonas;
+        private readonly IMapper _mapper;
+
+        //Constructor
+        public PersonaControllerAPI(iPersonaRepository ctoPersonas,  IMapper mapper)
+        {
+            _ctopersonas = ctoPersonas;
+            _mapper = mapper;
+        }
+    }
+}
